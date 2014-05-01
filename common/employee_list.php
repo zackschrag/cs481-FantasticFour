80d431c0d813ea92f3d4f442ec8225c26c86214c
jino <jinopark@users.noreply.github.com>
2014-05-01 16:07:06 -0600

    finished employeelist generator

--- common/employee_list.php
+++ common/employee_list.php
@@ -4,7 +4,11 @@
 		"Brickman, Bill" => 10,
 		"Charles, Linda" => 30,
 		"Danforth, Kyle" => 80,
-		"Gregory, Karen" => 100,
+		"Employee 5" => 25,
+		"Employee 6" => 50,
+		"Employee 7" => 5,
+		"Employee 8" => 90,
+
 		
 	);
 
@@ -12,13 +16,22 @@
 		echo '<div class="employee-sidebar-item">';
 		echo '<div class="employee-sidebar-item-name">'.$name.'</div>';
 		echo '<div class="progress">';
-		echo '<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: '.$progress.'%">';
+
+
+		$progessBarColor;
+		if ($progress == 100) $progessBarColor = "progress-bar progress-bar-success";
+		else if ($progress > 40) $progessBarColor = "progress-bar progress-bar-warning";
+		else $progessBarColor="progress-bar progress-bar-danger";
+
+
+		echo '<div class="'.$progessBarColor.'" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: '.$progress.'%">';
 		echo '<span class="sr-only">40% Complete (success)</span>';
-		echo "</div>"."\n";
-		echo "</div>"."\n";
-		echo "</div>"."\n";
+		echo "</div>";
+		echo "</div>";
+		echo "</div>";
 	}
 
 ?>
+
 
 
--- index.php
+++ index.php
@@ -13,19 +13,18 @@
 			</div>
 
 			<div class="col-lg-9 col-md-9">
-				
+
 				<table class="table table-bordered">
 					<thead>
 						<h3>Recent Activity</h3>
 					</thead>
 					<tbody>
 						<tr>
-							<td class="row-label">4/14/14</td>
+							<td class="row-label" rowspan="2">4/14/14</td>
 							<td>4:55 am</td>
 							<td>Nothing has happened yet.</td>
 						</tr>
 						<tr>
-							<td></td>
 							<td>4:54 am</td>
 							<td>Something will happen.</td>
 						</tr>
@@ -37,55 +36,6 @@
 					</tbody>
 				</table>
 				
-				
-				<!--
-				<div class="content-title">
-					<h3 class="text-center">John Alabaster</h3>
-				</div>
-				<div class="btn-group btn-group-justified add-employee">
-				  <div class="btn-group">
-				    <button type="button" class="btn btn-primary">Add employee</button>
-				  </div>
-				</div>
-				<div class="row">
-					<div class="col-lg-12 col-md-12">
-						<h4>Employee Information</h4>
-						<table class="table table-striped">
-							<thead>
-							</thead>
-							<tbody>
-								<tr>
-									<td class="row-label">First Name</td>
-									<td>John</td>
-								</tr>
-								<tr>
-									<td class="row-label">Last Name</td>
-									<td>Alabaster</td>
-								</tr>
-								<tr>
-									<td class="row-label">Job Title</td>
-									<td>Software Developer</td>
-								</tr>
-								<tr>
-									<td class="row-label">Benefits Package</td>
-									<td>Package A</td>
-								</tr>
-								<tr>
-									<td class="row-label">Pay Rate</td>
-									<td>$20 per hour</td>
-								</tr>
-								<tr>
-									<td class="row-label">Vacation Accrued</td>
-									<td>25 hours</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-					<div class="col-lg-6 col-md-6">
-						
-					</div>
-				</div>
-				-->
 			</div>
 		</div>
 	</div>
