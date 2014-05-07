70905c6b8c4586006bfbc80b4aa36bd3ca2a6374
jino <jinopark@users.noreply.github.com>
2014-05-07 15:28:27 -0600

    Employee altered

--- common/functions/showEmployeeList.php
+++ common/functions/showEmployeeList.php
@@ -1,11 +1,24 @@
 <?php
+$ctr = 0;
 foreach($employees as $name => $emp){
+
+	if ($ctr==0){
+		echo '<div class="employee-sidebar-item"><div class="formerEmpSHIT"><div class="checkbox">
+						    <label>
+						      <input type="checkbox"> Show Former Employees
+						    </label>
+						  </div></div></div>';
+		
+	}
+
+	
 		echo '<a href="viewEmployee.php?employee='.$name.'">';
-		echo '<div class="employee-sidebar-item">';
+		if ($ctr==0){echo '<div class="employee-sidebar-formeremp">';}
+		else {echo '<div class="employee-sidebar-item">';}
 		echo '<div class="employee-sidebar-item-name">'.$emp->lastName.", ".$emp->firstName.'</div>';
 		echo '<div class="progress">';
-
-
+		
+		
 		$progessBarColor;
 		if ($emp->formProgress == 100) $progessBarColor = "progress-bar progress-bar-success";
 		else if ($emp->formProgress > 40) $progessBarColor = "progress-bar progress-bar-warning";
@@ -18,6 +31,7 @@ foreach($employees as $name => $emp){
 		echo "</div>";
 		echo "</div>";
 		echo '</a>';
+		++$ctr;
 	}
 ?>
 
